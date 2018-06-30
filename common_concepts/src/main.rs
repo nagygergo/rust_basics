8ab326fd4142e0dd53c887a57a40d810b78206f7
Nagy Gergely <nagy.gergely14@gmail.com>
2018-06-30 12:46:07 +0200

    Chapter 3.2 Data types

--- common_concepts/src/main.rs
+++ common_concepts/src/main.rs
@@ -43,5 +43,54 @@ fn main() {
 
     println!("The number of spaces in the spaces variable is: {}", spaces);
 
+    //
+    // ─── DATA TYPES ─────────────────────────────────────────────────────────────────
+    //
+
+    //We need to provide the type to parse to. Would error without u32 type
+    let parsed_number_from_string: u32 = "42".parse().expect("Not a number!");
+
+    println!("parsed_number_from_string is: {}", parsed_number_from_string);
+
+
+    //Integer literals
+
+    // let integer_literal_decimal = 922_222;
+    // let integer_literal_hex = 0xff;
+    // let integer_literal_octal = 0o77;
+    // let integer_literal_binary = 0b1111_0000;
+
+    //Numeric Operations
+
+    let calc = 5.0 + 3.0 - 3.5 / 2.2 * 4.0 % 5.0;
+
+    println!("numeric operations calc is: {}", calc);
+
+    //Boolean type
+
+    let boolean_type: bool = true;
+
+    println!("boolean type value {}", boolean_type );
+
+    //Character type
+
+    let heart_eyed_cat = '😻';
     
+    println!("Character type: {}", heart_eyed_cat );
+
+    //Compound types
+
+    //Tuple type
+
+    //let tuple: (i32, f64, u8) = (500, 6.4, 1);
+
+    //Array type
+
+    let array = [1, 2, 3, 4, 5];
+    let index = 2;
+
+    let element = array[index];
+
+    println!("The value array element is: {}", element);
+
 }
